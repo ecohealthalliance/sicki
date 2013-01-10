e1811d033a1ebb8a363814b753a5fcc32fe8813b
Zack Krejci <zack.krejci@gmail.com>
2013-01-10 19:40:10 +0000

    removed extra db calls from controllers

--- static/js/sort.js
+++
@@ -1,8 +0,0 @@
-$ (document).ready (function () {
-    $ ('#sort_events').change (function () {
-	var val = $ (this).val ();
-	var base = '{{= URL (r = request) }}';
-	if (val.length)
-	    base += '?sort=' + val
-	window.location.href = base;
-});
