80f0e18121004ea2a2083c59f165cd24978b14b6
Zack Krejci <zack.krejci@gmail.com>
2013-01-13 05:35:55 +0000

    revamped proposal system, added admin mode, split events into multiple pages, fixed sorting field problem

--- controllers/eid.py
+++ controllers/eid.py
@@ -1,12 +1,44 @@
 # GET /sicki/eid/
 @require_logged_in
 def index():
-    sort = request.vars.get ('sort')
+    sort = request.vars.get ('sort') or 'event_name'
     return {'events': get_all_events (sort), 'sort': sort}
 
 @require_logged_in
 def fields():
     return {'event_fields': event_fields}
+
+@require_logged_in
+def stats():
+    eid_id = request.args (0)
+    if not eid_id:
+        raise HTTP (400, "Missing EID ID")
+    return {'event': get_event (eid_id), 'proposals': get_proposals (eid_id, auth.user.id)}
+
+@require_logged_in
+def event_map():
+    eid_id = request.args (0)
+    if not eid_id:
+        raise HTTP (400, "Missing EID ID")
+    return {'event': get_event (eid_id)}
+
+@require_logged_in
+def wiki():
+    eid_id = request.args (0)
+    if not eid_id:
+        raise HTTP (400, "Missing EID ID")
+    page = load_page (eid_id)
+    if not page:
+        insert_page (eid_id)
+        page = load_page (eid_id)
+    return {'event': get_event (eid_id), 'page': page}
+
+@require_logged_in
+def event_refs():
+    eid_id = request.args (0)
+    if not eid_id:
+        raise HTTP (400, "Missing EID ID")
+    return {'event': get_event (eid_id)}
 
 # GET /sicki/eid/view/<eid_id>
 @require_logged_in
@@ -40,19 +72,30 @@ def proposals():
 def propose():
     eid_id = request.args (0)
     field = request.args (1)
-    value = json.loads (request.vars.get ('val'))
+    
+    value = json.loads (request.vars.get ('value'))
     refs = json.loads (request.vars.get ('refs'))
     user = json.loads (request.vars.get('user'))
     date = json.loads (request.vars.get('date'))
-    #ensure_prop_eid (eid_id)
-    id = propose_edit (eid_id, field, value, refs, user, date)
-    return id
 
-# POST /sicki/eid/unpropose/<prop_id>
+    # Automatically accept proposals by admins
+    if has_role (admin_role):
+        edit_field (eid_id, field, value)
+        return 1
+    else:
+        prop_id = propose_edit (eid_id, field, value, refs, user, date)
+        return prop_id
+
+# POST /sicki/eid/reject/<prop_id>
 # Removes a proposal completely from the database
-@require_logged_in
-def unpropose():
-    pass
+@require_role (admin_role)
+def reject():
+    prop_id = request.args (0)
+    prop = get_proposal (prop_id)
+    if not prop:
+        raise HTTP (400)
+    remove_proposal (prop_id)
+    return 1
 
 # POST /sicki/eid/accept/<prop_id>
 # Accept a proposal. The proposal is removed and the corresponding field is modified.
@@ -65,7 +108,8 @@ def accept():
         raise HTTP (400)
     edit_field (str (prop['eid']), prop['field'], prop['value'])
     remove_proposal (prop_id)
-    redirect (URL (r = request, c = 'eid', f = 'view', args = [str (prop['eid'])]))
+    #redirect (URL (r = request, c = 'eid', f = 'view', args = [str (prop['eid'])]))
+    return 1
 
 # POST /sicki/eid/upvote/<prop_id>
 @require_logged_in
--- views/eid/fields.js
+++ views/eid/fields.js
@@ -4,3 +4,7 @@ var event_lookup = {};
 $.each (event_fields, function (i, field) {
     event_lookup[field.name] = field;
 });
+
+var user_id = {{= auth.user.id }};
+var admin_role = {{= ('true' if has_role (admin_role) else 'false') }};
+var application = '{{= request.application }}';
