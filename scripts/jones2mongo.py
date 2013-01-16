de7b4e57d70613d26540b120cc89190f5a84769e
Zack Krejci <zack.krejci@gmail.com>
2013-01-16 12:29:27 -0500

    added overview

--- scripts/jones2mongo.py
+++ scripts/jones2mongo.py
@@ -1,13 +1,13 @@
-#!/usr/local/bin/python
 import sys, csv, pymongo, json
 from pprint import pprint
 
 # give filename (eg ~/Dropbox/sicki/match-key.csv) on cmd line
 file = sys.argv[1]
 
-from pymongo import MongoClient
-connection = MongoClient()
-db = connection.sicki
+#from pymongo import MongoClient
+#connection = MongoClient()
+#db = connection.sicki
+db = pymongo.Connection ('localhost', 27017)['sicki']
 events = db.events
 
 # 'r' is for read mode
@@ -19,6 +19,7 @@ with open(file,'r') as csvfile:
         except ValueError:
             continue
         row['Sources'] = json.loads(row['Sources'])
+        print events.find_one({'eid_id':eid_id})
         events.update({'eid_id':eid_id},{ '$set' : {'jones':row} })
 
         
