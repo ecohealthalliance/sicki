776c93e4d3b1ab19043e5937c6bda23c5df7ff46
mgibson <gusgibson@yahoo.com>
2013-05-31 11:29:24 -0400

    comments and untabify get rid of those darn tabs!

--- static/js/proposal.js
+++ static/js/proposal.js
@@ -7,25 +7,25 @@ function Proposal (prop, stat) {
     // The upvote button
     var up_src = (prop.voted == 1) ? 'upvote_green.png' : 'upvote.png';
     upvote = $ ('<img class="vote" src="/' + application + '/static/images/' + up_src + '" />').click (function () {
-	$.ajax ({
-	    url: '/' + application + '/eid/upvote/' + prop.id,
-	    success: function () {
-		upvote.attr ('src', '/' + application + '/static/images/upvote_green.png');
-		downvote.attr ('src', '/' + application + '/static/images/downvote.png');
-	    }
-	});
+        $.ajax ({
+            url: '/' + application + '/eid/upvote/' + prop.id,
+            success: function () {
+                upvote.attr ('src', '/' + application + '/static/images/upvote_green.png');
+                downvote.attr ('src', '/' + application + '/static/images/downvote.png');
+            }
+        });
     });
     
     // The downvote button
     var down_src = (prop.voted == -1) ? 'downvote_red.png' : 'downvote.png';
     downvote = $ ('<img class="vote" src="/' + application + '/static/images/' + down_src + '" />').click (function () {
-	$.ajax ({
-	    url: '/' + application + '/eid/downvote/' + prop.id,
-	    success: function () {
-		upvote.attr ('src', '/' + application + '/static/images/upvote.png');
-		downvote.attr ('src', '/' + application + '/static/images/downvote_red.png');
-	    }
-	});
+        $.ajax ({
+            url: '/' + application + '/eid/downvote/' + prop.id,
+            success: function () {
+                upvote.attr ('src', '/' + application + '/static/images/upvote.png');
+                downvote.attr ('src', '/' + application + '/static/images/downvote_red.png');
+            }
+        });
     });
 
     // The container for the proposal value
@@ -36,41 +36,45 @@ function Proposal (prop, stat) {
     // The container for the references
     var refs =  $ ('<div class="refs"></div>');
     if (prop.refs.length > 0)
-	refs.append ('<div class="subtitle">References</div>');
+        refs.append ('<div class="subtitle">References</div>');
     $.each (prop.refs, function (i, ref) {
-	refs.append ($ ('<div class="ref"></div>').text (ref.name));
+        refs.append ($ ('<div class="ref"></div>').text (ref.name));
     });
 
     var main = $ ('<div class="prop"></div>');
     main.append (wrapper).append (refs);
 
-    // The admin controls
+    // The admin controls. admin hits accept and ajax call is made to server to
+    // save proposal
     var controls = $ ('<div></div>');
     var accept = $ ('<a class="control" href="#">Accept</a>"').click (function () {
-	$.ajax ({
-	    url: '/' + application + '/eid/accept/' + prop.id,
-	    success: function () {
-		stat.update (prop.value);
-		stat.remove_proposal (that.id);
-		//main.remove ();
-		//commit_value (prop.field, prop.value);
-	    }
-	});
-	
-	return false;
+        $.ajax ({
+            url: '/' + application + '/eid/accept/' + prop.id,
+            success: function () {
+                // update client view with proposal
+                stat.update (prop.value);
+                // and remove now accepted proposal
+                stat.remove_proposal (that.id);
+                //main.remove ();
+                //commit_value (prop.field, prop.value);
+            }
+        });
+
+        return false;
     });
+    // admin rejects
     var reject = $ ('<a class="control" href="#">Reject</a>"').click (function () {
-	$.ajax ({
-	    url: '/' + application + '/eid/reject/' + prop.id,
-	    success: function () {
-		stat.remove_proposal (that.id);
-		//main.remove ();
-	    }
-	});
-	return false;
+        $.ajax ({
+            url: '/' + application + '/eid/reject/' + prop.id,
+            success: function () {
+                stat.remove_proposal (that.id);
+                //main.remove ();
+            }
+        });
+        return false;
     });
     if (admin_role) {
-	controls.append (accept).append (reject);
+        controls.append (accept).append (reject);
     }
     main.append (controls);
 
