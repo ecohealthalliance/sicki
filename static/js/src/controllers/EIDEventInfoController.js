b69c6704811c90530fffa08921d87e1fe9a80936
Zack Krejci <zack.krejci@gmail.com>
2013-06-04 19:59:57 -0400

    moved model loading call somewhere more sane

--- static/js/src/controllers/EIDEventInfoController.js
+++ static/js/src/controllers/EIDEventInfoController.js
@@ -1,19 +1,23 @@
 define(['jquery', 'sicki/controllers/Controller', 'sicki/models/EIDEvent'], function($, Controller, EIDEvent) {
 
-    var EIDEventController = Controller.extend({
-        Model: EIDEvent,
+    var EIDEventInfoController = Controller.extend({
+        initialize: function(options) {
+            this.render();
+        },
 
         overview: function() {
             console.log('overview', this.model.get('id'));
         },
 
-        info: function() {
-            $('#main').html(sicki.views.eid.info({
+        render: function() {
+            this.$el.html(sicki.views.eid.info({
                 eidEvent: this.model.toJSON()
             }));
         }
+    }, {
+        Model: EIDEvent,
     });
 
-    return new EIDEventController();
+    return EIDEventInfoController;
 
 });
--- static/js/src/routes/Router.js
+++ static/js/src/routes/Router.js
@@ -7,9 +7,20 @@ define(['require', 'backbone'], function(require, Backbone) {
         },
 
         viewEID: function(eid_id, view) {
-            require(['../controllers/EIDEventController'], function(EIDEventController) {
-                EIDEventController.switchView(eid_id, view);
-            });
+            if (this.controller)
+                this.controller.remove();
+
+            if (view == 'info') {
+                require(['sicki/models/EIDEvent', 'sicki/controllers/EIDEventInfoController'], function(EIDEvent, EIDEventInfoController) {
+                    EIDEventInfoController.create(eid_id, {
+                        el: '#main'
+                    }, function(controller) {
+                        this.controller = controller;
+                    }.bind(this));
+                });
+            }
+            else
+                throw "Not Implemented";
         },
 
         viewEIDList: function() {
