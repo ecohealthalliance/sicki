f78a657d41b89ef5d45e5538984eb9242617b9ac
Zack Krejci <zack.krejci@gmail.com>
2013-06-05 15:50:25 -0400

    adding editing stuff

--- static/built/views/eid/field.js
+++ static/built/views/eid/field.js
@@ -1,8 +1,8 @@
 define(['jade'],function(jade){return function anonymous(locals) {
 var buf = [];
-var locals_ = (locals || {}),block = locals_.block,attributes = locals_.attributes,escaped = locals_.escaped,field = locals_.field,model = locals_.model;var text_mixin = function(field, value){
+var locals_ = (locals || {}),block = locals_.block,attributes = locals_.attributes,escaped = locals_.escaped,field = locals_.field,text_mixin = locals_.text_mixin,model = locals_.model;var textField_mixin = function(field, value){
 var block = this.block, attributes = this.attributes || {}, escaped = this.escaped || {};
-buf.push("<span class=\"name\">" + (jade.escape(null == (jade.interp = field.name + ': ') ? "" : jade.interp)) + "</span><span class=\"value\">" + (jade.escape(null == (jade.interp = value) ? "" : jade.interp)) + "</span>");
+buf.push("<div><span class=\"name\">" + (jade.escape(null == (jade.interp = field.name + ': ') ? "" : jade.interp)) + "</span><span class=\"value\">" + (jade.escape(null == (jade.interp = value) ? "" : jade.interp)) + "</span></div><div class=\"edit\"><input" + (jade.attrs({ 'value':(value) }, {"value":true})) + "/></div>");
 };
 buf.push("<div>");
 if ( field.type == 'text' || field.type === undefined)
