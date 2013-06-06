81e30409a0e3211bb2c9103b355c939470e76dd1
Zack Krejci <zack.krejci@gmail.com>
2013-06-05 20:55:02 -0400

    added listening and appending functionality to the controller base class, and documentation

--- static/src/controllers/Controller.js
+++ static/src/controllers/Controller.js
@@ -1,14 +1,55 @@
+/* This module defines a Controller base class. To subclass, you will need to extend
+ * some key properties.
+ *
+ * 
+ * Model, a static property, specifies the constructor for model that this controller 
+ * will use
+ * 
+ * view is the template function. It should return and html string
+ * 
+ * viewArgs specifies what parameters to pass to the view functions. It returns an 
+ * object
+ *
+ * subControllers defines a mapping between DOM elements in this view and descendant 
+ * controllers. These subControllers will be rendered whenever the parent view is
+ * rendered
+ *
+ * listeners is a mapping that defines under what circumstances the view should be 
+ * updated
+ *    
+ */ 
 define(['backbone'], function(Backbone) {
     var Controller = Backbone.View.extend({
+        // The model this controller uses
         model: null,
 
+        // A mapping between selectors and subcontrollers of the controller
         subControllers: {},
 
+        // A mapping between events's to listen for and objects to listen to
+        listeners: {},
+
+        // The main view of this controller
+        view: function() {
+            return '';
+        },
+
+        // the arguments to pass to the view of this controller
+        viewArgs: function() {
+            return {
+                model: model.toJSON()
+            };
+        },
+
         initialize: function(options) {
-            if (options.model)
-                options.model.on('change', function() {
-                    this.render();
-                }.bind(this));
+            _.extend(this, options);
+            if (options.model) {
+                for (var event in this.listeners) {
+                    this.listeners[event].on(event, function() {
+                        this.render();
+                    }.bind(this));
+                }
+            }
             this.render();
         },
 
@@ -19,6 +60,7 @@ define(['backbone'], function(Backbone) {
         },
 
         render: function() {
+            this.$el.html(this.view(this.viewArgs()));
             for (var selector in this.subControllers) {
                 this.subControllers[selector].setElement(this.$(selector)).render();
             }
